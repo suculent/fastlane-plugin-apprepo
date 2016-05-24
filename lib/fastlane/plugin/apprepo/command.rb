e18d9016924811f429aeaff4e4f91c44881c12f3
Matěj Sychra <igraczech@igraczech.com>
2016-05-24 18:12:21 +0200

    empty command file

---
+++ lib/fastlane/plugin/apprepo/command.rb
@@ -0,0 +1,13 @@
+module Apprepo
+  module Command    
+    class Make
+      def run
+        fputs 'make run executed'
+      end
+    end
+    
+    def run
+        fputs 'legacy run executed'
+    end
+  end
+end
