86c9cd99dbf3f13c1c2e5f35fe9113363203395b
Matěj Sychra <matej.sychra@t-mobile.cz>
2018-08-22 10:54:26 +0200

    version bump, updated lockfile

--- lib/fastlane/plugin/apprepo/version.rb
+++ lib/fastlane/plugin/apprepo/version.rb
@@ -1,7 +1,7 @@
 module Fastlane
   # Static configuration values
   module Apprepo
-    VERSION = "0.4.3".freeze
+    VERSION = "0.5.0".freeze
     SUMMARY = 'Fastlane plugin for distributing artifacts with SFTP upload'.freeze
     DESCRIPTION = 'Fastlane plugin SFTP/AppRepo uploader'.freeze
   end
