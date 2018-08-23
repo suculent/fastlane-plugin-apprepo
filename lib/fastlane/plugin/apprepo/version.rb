78e674b5ce3116ef041cb1c139063fd195961a57
Matěj Sychra <matej.sychra@t-mobile.cz>
2018-08-23 15:03:38 +0200

    coverage fix by removing invalid files for gem checkout

--- lib/fastlane/plugin/apprepo/version.rb
+++ lib/fastlane/plugin/apprepo/version.rb
@@ -1,7 +1,7 @@
 module Fastlane
   # Static configuration values
   module Apprepo
-    VERSION = "0.5.3".freeze
+    VERSION = "0.5.4".freeze
     SUMMARY = 'Fastlane plugin for distributing artifacts with SFTP upload'.freeze
     DESCRIPTION = 'Fastlane plugin SFTP/AppRepo uploader'.freeze
   end
